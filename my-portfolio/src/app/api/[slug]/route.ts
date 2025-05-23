1fb5a0ed5c02f079acad9841b90c26cc43adb6ee
Walter Santiago <waltersantiago@UBVPNN3013.local>
2025-05-23 23:33:23 +0800

    Add API endpoint
    As a user, I want to retrieve a candidate's contact details, work experience, and skills via API, so I can efficiently integrate this data in our app

---
+++ my-portfolio/src/app/api/[slug]/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+import contact from '@/data/contact.json'
+import experience from '@/data/experience.json'
+import skills from '@/data/skills.json'
+
+export async function GET(
+    request: NextRequest,
+    { params }: { params: { slug: string } },
+) {
+    try {
+        const { slug } = params
+
+        if (slug === 'contact') {
+            return NextResponse.json(contact)
+        }
+
+        if (slug === 'experience') {
+            return NextResponse.json(experience)
+        }
+
+        if (slug === 'skills') {
+            return NextResponse.json(skills)
+        }
+
+        return NextResponse.json({ mesage: 'Error' })
+    } catch {}
+}
