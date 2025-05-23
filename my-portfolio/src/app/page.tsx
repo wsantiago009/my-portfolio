d1672c1a85b40238087c566e07fbdcfc2a59a090
Walter Santiago <waltersantiago@UBVPNN3013.local>
2025-05-23 22:35:13 +0800

    Add resume canvass

---
+++ my-portfolio/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+    return (
+        <div className="min-h-screen bg-gradient-to-br from-gray-400 to-gray-100 flex py-4 justify-center tracking-wide">
+            <div className="bg-white container max-w-3xl shadow-lg text-gray-500"></div>
+        </div>
+    )
+}
