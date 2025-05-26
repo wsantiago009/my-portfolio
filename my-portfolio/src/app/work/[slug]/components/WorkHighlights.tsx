df5dc1bc934966dbfaca8c927a2d8f395b326e97
Walter Santiago <waltersantiago@UBVPNN3013.local>
2025-05-27 00:11:11 +0800

    Add project thumbnail
    As a user, I want to browse visual thumbnails of a talent's key projects so I can quickly assess their work quality and relevance to our needs.

--- my-portfolio/src/app/work/[slug]/components/WorkHighlights.tsx
+++ my-portfolio/src/app/work/[slug]/components/WorkHighlights.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { WorkHighlight } from '@/types'
 
 import FadeInWrapper from '@/components/FadeInWrapper'
@@ -6,12 +7,24 @@ const WorkHighlights = ({ data }: { data: WorkHighlight[] }) => {
     return (
         <div className="px-14 py-10">
             {data?.map((item, i) => (
-                <FadeInWrapper
-                    key={i}
-                    className="mb-12"
-                >
+                <FadeInWrapper key={i} className="mb-12">
                     <h4 className="text-3xl mb-4">{item.project}</h4>
-                    <ul className='pl-4'>
+                    {item.thumbnail && (
+                        <div className="mb-4">
+                            <Image
+                                alt={item.project}
+                                src={item.thumbnail}
+                                width={800}
+                                height={600} 
+                                sizes="100vw"
+                                style={{
+                                    width: '100%',
+                                    height: 'auto',
+                                }}
+                            />
+                        </div>
+                    )}
+                    <ul className="pl-4">
                         {item.highlight.map((item, j) => (
                             <li key={j} className="mb-3">
                                 - {item}
--- my-portfolio/src/types/index.d.ts
+++ my-portfolio/src/types/index.d.ts
@@ -14,5 +14,6 @@ export type Experience = {
 
 export type WorkHighlight = {
     project: string
+    thumbnail: string
     highlight: string[]
 }
