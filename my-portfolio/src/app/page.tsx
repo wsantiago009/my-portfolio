6b7d66edf1aedd7a0ed7919bdd8f0e9d91ab207d
Walter Santiago <waltersantiago@UBVPNN3013.local>
2025-07-14 18:10:22 +0800

    Add homepage

--- my-portfolio/src/app/page.tsx
+++ my-portfolio/src/app/page.tsx
@@ -1,63 +1,36 @@
-import { getFetchApi } from '@/utils/fetchApis'
-
-import PaperCanvass from '@/components/Resume/PaperCanvass'
-import ContactElem from '@/components/Resume/ContactList'
-import ExperienceList from '@/components/Resume/ExperienceList'
-import SkillList from '@/components/Resume/SkillList'
-
-import { Contact, Experience } from '@/types'
+import Image from 'next/image'
 
 export default async function Home() {
-    const contactData: Contact[] = await getFetchApi(
-        'http://localhost:3000/api/contact',
-    )
-    const experienceData: Experience[] = await getFetchApi(
-        'http://localhost:3000/api/experience',
-    )
-    const skillsData: String[] = await getFetchApi(
-        'http://localhost:3000/api/skills',
-    )
-
     return (
-        <PaperCanvass>
-            <div className="p-10 md:p-16 md:pb-10">
-                <h3 className="text-3xl mb-2 text-gray-700">WALTER SANTIAGO</h3>
-                <p className="text-sm font-thin mb-4">
-                    SENIOR FRONT-END DEVELOPER
-                </p>
-                <p className="text-sm">
-                    Software developer with over 10 years of experience building
-                    web apps in React JS for millions of users, catering to both
-                    business and individual needs. Proven in agile environments,
-                    delivering high-quality, scalable solutions through strong
-                    collaboration.
-                </p>
+        <div className="flex justify-center max-w-screen-lg mx-auto gap-5">
+            <div className="w-full max-w-md">
+                <Image
+                    src="https://placehold.co/800x400/png"
+                    alt="profile"
+                    width={800}
+                    height={400}
+                    sizes="100vw"
+                    style={{
+                        width: '100%',
+                        height: 'auto',
+                    }}
+                />
             </div>
-            <div className="bg-gray-200 px-10 md:px-16 sm:flex sm:justify-between sm:items-center">
-                {contactData &&
-                    contactData?.map((item: Contact, i) => (
-                        <ContactElem key={i} content={item} />
-                    ))}
-            </div>
-            <div className="grid grid-cols-12 gap-4 px-10 md:px-16 py-10">
-                <div className="col-span-12 md:col-span-4 md:col-start-9 md:row-start-1 md:pl-7 pb-5 mb-5 md:border-b-0 border-b">
-                    {skillsData && (
-                        <>
-                            <h4 className="text-2xl text-gray-700 mb-4">
-                                SKILLS
-                            </h4>
-                            <SkillList content={skillsData} />
-                        </>
-                    )}
+            <div className="flex flex-col justify-between py-3">
+                <div className="w-full">
+                    <h2 className="text-5xl font-bold text-gray-900 mb-4">
+                        Frontend Developer
+                    </h2>
+                    <p>
+                        I'm a frontend developer specializing in React,
+                        JavaScript, and building user-friendly interfaces.
+                        Explore my projects to see my work.
+                    </p>
                 </div>
-                <div className="col-span-12 md:col-span-8 md:col-start-1 md:row-start-1 md:pr-8 md:border-r">
-                    <h4 className="text-2xl text-gray-700 mb-4">EXPERIENCE</h4>
-                    {experienceData &&
-                        experienceData?.map((item: Experience, i) => (
-                            <ExperienceList key={i} content={item} />
-                        ))}
-                </div>
+                <button className="bg-blue-600 text-white w-full p-2 rounded-lg">
+                    View Projects
+                </button>
             </div>
-        </PaperCanvass>
+        </div>
     )
 }
---
+++ my-portfolio/src/components/layout/Header.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+const Header = () => {
+    return (
+        <header className="bg-white py-5 shadow-sm">
+            <div className="max-w-screen-xl mx-auto flex justify-between">
+                <h4>My Portfolio</h4>
+                <nav className="space-x-7">
+                    <Link href="/projects">Project</Link>
+                    <Link href="/experience">Experience</Link>
+                    <Link href="/resume" className='p-3 bg-blue-200 rounded-lg text-gray-700 font-semibold'>Resume</Link>
+                </nav>
+            </div>
+        </header>
+    )
+}
+export default Header
