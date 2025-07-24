10126856175e6581f27f8d147a7e9a2e16474aa9
Walter Santiago <w.santiago009@gmail.com>
2025-07-24 17:20:32 +0800

    Fix issue in user.email

--- my-portfolio/src/components/Resume/SkillList.tsx
+++ my-portfolio/src/components/Resume/SkillList.tsx
@@ -2,7 +2,7 @@ const SkillList = ({ content }: { content: string[] }) => {
     return (
         <ul className="skills-wrapper text-sm md:pl-3">
             {content?.map((item, i) => (
-                <li key={i} className="md:list-disc mb-1 md:list-item inline-block mr-1">
+                <li key={i} className="md:list-disc mb-1 md:list-item inline-block mr-2">
                     {item}
                 </li>
             ))}
