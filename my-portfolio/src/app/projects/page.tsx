812126209437827dca9fa5a60b669dbe50ee6484
Walter Santiago <w.santiago009@gmail.com>
2025-08-20 19:53:47 +0800

    Enhance project page

--- my-portfolio/src/app/projects/page.tsx
+++ my-portfolio/src/app/projects/page.tsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link'
 import Image from 'next/image'
+import { MoveRight } from 'lucide-react'
 
 import { useProjectsContext } from '@/context/ProjectsContext'
 
@@ -23,7 +24,10 @@ export default function Page() {
                     const slugUrl = item?.slug || projectName
 
                     return (
-                        <div className="col-span-4" key={i}>
+                        <div
+                            className="group lg:col-span-4 sm:col-span-6 col-span-12 mb-4"
+                            key={i}
+                        >
                             <Link href={`projects/${slugUrl?.toLowerCase()}`}>
                                 <div className="mb-4">
                                     <Image
@@ -43,7 +47,16 @@ export default function Page() {
                                     <h5 className="text-xl font-semibold mb-1">
                                         {projectName}
                                     </h5>
-                                    <p>{item?.description}</p>
+                                    <p className="line-clamp-4">
+                                        {item?.description}
+                                    </p>
+                                </div>
+                                <div className="flex items-center my-6">
+                                    <p className="font-bold text-sm mr-1">View Project</p>
+                                    <MoveRight
+                                        size={14}
+                                        className="content-center transition-transform duration-300 group-hover:translate-x-1"
+                                    />
                                 </div>
                             </Link>
                         </div>
